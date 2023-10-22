a59f9d6552d81777d9eda0012df02a844e1f89f6
Shijal Tarmali <126109257+Shijaltarmali@users.noreply.github.com>
2023-10-22 12:29:42 -0600

    Update setup.py
    py version

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.9',
   ],
 )
